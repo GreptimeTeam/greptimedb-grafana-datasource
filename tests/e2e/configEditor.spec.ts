2e98368b93030777b071c10ac4948f33eb7c1ca7
scl <sunchang_long@163.com>
2025-04-23 11:31:28 +0800

    fijx e2e test

--- tests/e2e/configEditor.spec.ts
+++ tests/e2e/configEditor.spec.ts
@@ -1,24 +1,17 @@
 import { expect, test } from '@grafana/plugin-e2e';
 
-const PLUGIN_UID = 'grafana-clickhouse-datasource';
-const CLICKHOUSE_DB_URL = Boolean(process.env.CI) ? 'clickhouse-server' : 'localhost';
+const PLUGIN_UID = 'info8fcc-greptimedb-datasource';
+const GreptimeDB_URL = 'http://host.docker.internal:4000';
 
 test.describe('Config Editor', () => {
-  test('invalid credentials should return an error', async ({ createDataSourceConfigPage, page }) => {
-    const configPage = await createDataSourceConfigPage({ type: PLUGIN_UID });
-    await page.getByPlaceholder('Server address').fill(CLICKHOUSE_DB_URL);
-    await expect(configPage.saveAndTest()).not.toBeOK();
-  });
-
   test('valid credentials should display a success alert on the page', async ({ createDataSourceConfigPage, page }) => {
     const configPage = await createDataSourceConfigPage({ type: PLUGIN_UID });
+    await page.locator('input[name="host"]').fill(GreptimeDB_URL);
 
-    await page.getByPlaceholder('Server address').fill(CLICKHOUSE_DB_URL);
-    await page.getByPlaceholder('9000').fill('9000');
-    await page.getByPlaceholder('default').fill('default');
-
-    await configPage.saveAndTest();
-    await expect(configPage).toHaveAlert('success', { hasNotText: 'Datasource updated' });
+    await configPage.saveAndTest({
+      path: ''
+    });
+    await expect(configPage).toHaveAlert('success');
 
     await page.pause();
   });
@@ -26,14 +19,10 @@ test.describe('Config Editor', () => {
   test('mandatory fields should show error if left empty', async ({ createDataSourceConfigPage, page }) => {
     const configPage = await createDataSourceConfigPage({ type: PLUGIN_UID });
 
-    await page.getByPlaceholder('Server address').fill('');
+    await page.locator('input[name="host"]').fill('');
     await page.keyboard.press('Tab');
     await expect(page.getByText('Server address required')).toBeVisible();
 
-    await page.getByPlaceholder('9000').fill('');
-    await page.keyboard.press('Tab');
-    await expect(page.getByText('Port is required')).toBeVisible();
-
-    await expect(configPage.saveAndTest()).not.toBeOK();
+    await expect(configPage.saveAndTest({path: ''})).not.toBeOK();
   });
 });
