dd89fa4f08200d5fb4c2444c31fede65fcac6fa9
scl <sunchang_long@163.com>
2025-04-28 14:30:05 +0800

    refactor: remove old files

--- pkg/plugin/instance/sql/querier.go
+++
@@ -1,2 +0,0 @@
-// TODO: Implement SQL Querier
-package sql
--- src/querybuilder/mysql/types.ts
+++
@@ -1,7 +0,0 @@
-import { SQLOptions, SQLQuery } from './sql/types';
-
-export interface MySQLOptions extends SQLOptions {
-  allowCleartextPasswords?: boolean;
-}
-
-export interface MySQLQuery extends SQLQuery {}
